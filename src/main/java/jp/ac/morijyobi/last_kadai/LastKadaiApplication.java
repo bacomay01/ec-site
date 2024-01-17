90966e5a3090bf2bf44b612f8f00a54fce854bff
n.h.long <nguyen.hoang.long.sys21@morijyobi.ac.jp>
2024-01-17 13:47:46 +0900

    init

---
+++ src/main/java/jp/ac/morijyobi/last_kadai/LastKadaiApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.morijyobi.last_kadai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastKadaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastKadaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jp/ac/morijyobi/last_kadai/LastKadaiApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.morijyobi.last_kadai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastKadaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
